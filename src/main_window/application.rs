78bb2c360aa7f55d9c5a6b90d383f250907bd5d8
Yū Kobayashi <kb10uy@kb10uy.org>
2022-04-15 15:27:32 +0900

    Show monitor name instead

--- src/main_window/application.rs
+++ src/main_window/application.rs
@@ -6,14 +6,16 @@ use crate::{
 use std::sync::Arc;
 
 use egui::{
-    menu, CentralPanel, Context as EguiContext, FontId, Grid, RichText, Style, TextStyle,
-    TopBottomPanel,
+    menu, CentralPanel, Color32, Context as EguiContext, FontId, Grid, Id, Pos2 as UiPos2, Rect,
+    Response, RichText, Sense, Stroke, Style, TextStyle, TopBottomPanel, Ui, Vec2 as UiVec2,
 };
 use epi::{App as EpiApp, Frame as EpiFrame, Storage as EpiStorage};
+use vek::{Vec2, Vec4};
 
 pub struct MainWindowApp {
     event_proxy: Option<Arc<EventProxy>>,
     monitors: Vec<Monitor>,
+    monitor_preview_rects: Vec<Vec4<f32>>,
     selected_monitor_index: Option<usize>,
 }
 
@@ -24,13 +26,64 @@ impl MainWindowApp {
 
     pub fn set_monitors(&mut self, monitors: Vec<Monitor>) {
         self.monitors = monitors;
-        self.selected_monitor_index = if self.monitors.len() > 0 {
-            Some(0)
-        } else {
+        self.selected_monitor_index = if self.monitors.is_empty() {
             None
+        } else {
+            Some(0)
         };
+
+        self.calculate_preview();
     }
 
+    /// Calculates normalized monitor preview rects.
+    fn calculate_preview(&mut self) {
+        self.monitor_preview_rects.clear();
+        if self.monitors.is_empty() {
+            return;
+        }
+
+        let (x_points, y_points): (Vec<_>, Vec<_>) = self
+            .monitors
+            .iter()
+            .flat_map(|m| {
+                let position = m.position();
+                let size = m.size();
+                [
+                    (position.x, position.y),
+                    (position.x + size.x, position.y + size.y),
+                ]
+            })
+            .unzip();
+
+        let left_all = *x_points.iter().min().expect("No monitor calculated") as f32;
+        let right_all = *x_points.iter().max().expect("No monitor calculated") as f32;
+        let top_all = *y_points.iter().min().expect("No monitor calculated") as f32;
+        let bottom_all = *y_points.iter().max().expect("No monitor calculated") as f32;
+
+        let position_all = Vec2::new(left_all, top_all);
+        let size_all = Vec2::new(right_all - left_all, bottom_all - top_all);
+        let divider = size_all.x.max(size_all.y);
+        let offset = (Vec2::new(divider, divider) - size_all) / 2.0;
+
+        for monitor in &self.monitors {
+            let raw_position = monitor.position().as_::<f32>();
+            let raw_size = monitor.size().as_::<f32>();
+
+            let normalized_position = (raw_position - position_all + offset) / divider;
+            let normalized_size = raw_size / divider;
+
+            self.monitor_preview_rects.push(Vec4::new(
+                normalized_position.x,
+                normalized_position.y,
+                normalized_position.x + normalized_size.x,
+                normalized_position.y + normalized_size.y,
+            ));
+        }
+    }
+}
+
+/// UI Actions
+impl MainWindowApp {
     /// Tells the window to quit.
     pub fn request_exit(&self) {
         if let Some(proxy) = &self.event_proxy {
@@ -49,6 +102,7 @@ impl Default for MainWindowApp {
         MainWindowApp {
             event_proxy: None,
             monitors: vec![],
+            monitor_preview_rects: vec![],
             selected_monitor_index: None,
         }
     }
@@ -80,26 +134,27 @@ impl EpiApp for MainWindowApp {
             });
         });
 
-        let selected_monitor = match self.selected_monitor_index {
-            Some(i) => &self.monitors[i],
+        let mut selected_index = match self.selected_monitor_index {
+            Some(i) => i,
             None => return,
         };
-        let selected_size = selected_monitor.size();
-        let selected_position = selected_monitor.position();
+        let selected_size = self.monitors[selected_index].size();
+        let selected_position = self.monitors[selected_index].position();
 
         CentralPanel::default().show(ctx, |ui| {
+            // Monitor preview & selection
+            ui.vertical_centered(|ui| {
+                self.ui_draw_monitor_preview(ui, 320.0, selected_index, &mut selected_index);
+            });
             ui.horizontal_wrapped(|ui| {
                 for (i, monitor) in self.monitors.iter().enumerate() {
-                    ui.selectable_value(
-                        &mut self.selected_monitor_index,
-                        Some(i),
-                        format!("Monitor #{}", i + 1),
-                    )
-                    .on_hover_text(monitor.id_as_string());
+                    ui.selectable_value(&mut selected_index, i, monitor.name())
+                        .on_hover_text(monitor.id_as_string());
                 }
             });
+            self.selected_monitor_index = Some(selected_index);
 
-            Grid::new("monitor_select")
+            Grid::new("monitor_info")
                 .num_columns(2)
                 .min_col_width(128.0)
                 .show(ui, |ui| {
@@ -122,5 +177,57 @@ impl EpiApp for MainWindowApp {
 
             egui::warn_if_debug_build(ui);
         });
+    }
+}
+
+/// UI Elements
+impl MainWindowApp {
+    /// Draws monitor preview rects.
+    fn ui_draw_monitor_preview(
+        &self,
+        ui: &mut Ui,
+        size: f32,
+        selected: usize,
+        target: &mut usize,
+    ) -> Response {
+        let (response, painter) = ui.allocate_painter(UiVec2::new(size, size), Sense::hover());
+        let rect_size = response.rect.size();
+        let multiplier = response.rect.width().min(response.rect.height());
+        let left_top = response.rect.left_top();
+        let offset = left_top + (rect_size - UiVec2::splat(multiplier)) / 2.0;
+
+        let stroke = Stroke::new(2.0, Color32::WHITE);
+        let stroke_selected = Stroke::new(2.0, Color32::BLUE);
+        let fill = Color32::from_white_alpha(32);
+        for (i, monitor_preview) in self.monitor_preview_rects.iter().enumerate() {
+            let mlt = UiPos2::new(
+                monitor_preview.x * multiplier + 2.0,
+                monitor_preview.y * multiplier + 2.0,
+            ) + offset.to_vec2();
+            let mrb = UiPos2::new(
+                monitor_preview.z * multiplier - 2.0,
+                monitor_preview.w * multiplier - 2.0,
+            ) + offset.to_vec2();
+            let monitor_rect = Rect::from_min_max(mlt, mrb);
+
+            painter.rect_filled(monitor_rect, 2.0, fill);
+
+            if i == selected {
+                painter.rect_stroke(monitor_rect, 2.0, stroke_selected);
+            } else {
+                painter.rect_stroke(monitor_rect, 2.0, stroke);
+            }
+
+            let monitor_response = ui.interact(
+                monitor_rect,
+                Id::new(format!("monitor_preview_{i}")),
+                Sense::click(),
+            );
+            if monitor_response.clicked() {
+                *target = i;
+            }
+        }
+
+        response
     }
 }
--- src/windows/wallpaper.rs
+++ src/windows/wallpaper.rs
@@ -1,14 +1,22 @@
 //! Provides desktop wallpaper manipulation.
 
-use std::{ffi::OsString, os::windows::prelude::OsStringExt, slice::from_raw_parts};
+use std::{
+    collections::HashMap, ffi::OsString, mem::size_of, os::windows::prelude::OsStringExt,
+    ptr::null, slice::from_raw_parts,
+};
 
 use anyhow::{Context, Result};
 use vek::Vec2;
 use windows::{
     core::PCWSTR,
     Win32::{
+        Foundation::BOOL,
+        Graphics::Gdi::{EnumDisplayDevicesW, DISPLAY_DEVICEW},
         System::Com::{CoCreateInstance, CLSCTX_ALL},
-        UI::Shell::{DesktopWallpaper, IDesktopWallpaper},
+        UI::{
+            Shell::{DesktopWallpaper, IDesktopWallpaper},
+            WindowsAndMessaging::EDD_GET_DEVICE_INTERFACE_NAME,
+        },
     },
 };
 
@@ -17,6 +25,9 @@ use windows::{
 pub struct Monitor {
     /// Monitor ID WSTR, which contains NUL word.
     id: Box<[u16]>,
+
+    /// Monitor name.
+    name: String,
 
     /// Top-left monitor position.
     position: Vec2<i32>,
@@ -29,6 +40,11 @@ impl Monitor {
     /// Gets monitor ID as `String`.
     pub fn id_as_string(&self) -> String {
         OsString::from_wide(&self.id).to_string_lossy().to_string()
+    }
+
+    /// Gets monitor name.
+    pub fn name(&self) -> &str {
+        &self.name
     }
 
     /// Gets monitor position.
@@ -61,6 +77,8 @@ impl Wallpaper {
 
     /// Fetches connected monitors information.
     pub fn monitors(&self) -> Result<Vec<Monitor>> {
+        let monitor_names = self.list_monitor_names()?;
+
         let monitor_count = unsafe { self.interface.GetMonitorDevicePathCount()? } as usize;
 
         let mut monitors = Vec::with_capacity(monitor_count);
@@ -80,10 +98,72 @@ impl Wallpaper {
             let rect = unsafe { self.interface.GetMonitorRECT(PCWSTR(id.as_ptr()))? };
             let position = Vec2::new(rect.left, rect.top);
             let size = Vec2::new(rect.right - rect.left, rect.bottom - rect.top);
+            let name = monitor_names
+                .get(&id)
+                .cloned()
+                .unwrap_or_else(|| format!("Monitor #{i}"));
 
-            monitors.push(Monitor { id, position, size })
+            monitors.push(Monitor {
+                id,
+                name,
+                position,
+                size,
+            })
         }
 
         Ok(monitors)
+    }
+
+    fn list_monitor_names(&self) -> Result<HashMap<Box<[u16]>, String>> {
+        let mut display_device = DISPLAY_DEVICEW {
+            cb: size_of::<DISPLAY_DEVICEW>() as u32,
+            ..Default::default()
+        };
+
+        let mut name_pairs = HashMap::new();
+        let mut index = 0;
+        loop {
+            let device_name = unsafe {
+                let hr = EnumDisplayDevicesW(PCWSTR(null()), index, &mut display_device, 0);
+                if hr == BOOL(0) {
+                    break;
+                }
+
+                display_device.DeviceName
+            };
+
+            unsafe {
+                let hr = EnumDisplayDevicesW(
+                    PCWSTR(device_name[..].as_ptr()),
+                    0,
+                    &mut display_device,
+                    EDD_GET_DEVICE_INTERFACE_NAME,
+                );
+                if hr == BOOL(0) {
+                    break;
+                }
+            }
+
+            let id_length = display_device
+                .DeviceID
+                .iter()
+                .position(|&x| x == 0)
+                .expect("Unterminated text");
+            let id: Box<[u16]> = display_device.DeviceID[..(id_length + 1)].into();
+
+            let string_length = display_device
+                .DeviceString
+                .iter()
+                .position(|&x| x == 0)
+                .expect("Unterminated text");
+            let name: String = OsString::from_wide(&display_device.DeviceString[..string_length])
+                .to_string_lossy()
+                .to_string();
+
+            name_pairs.insert(id, name);
+            index += 1;
+        }
+
+        Ok(name_pairs)
     }
 }
